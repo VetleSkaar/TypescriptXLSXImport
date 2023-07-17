aef173652da39f0f63a7a0bf559cdcb5617c64d2
Vetle Skaar <vetleskaar99@gmail.com>
2023-07-17 11:03:41 +0200

    feat: Basic reader

--- typescript-xlsx/app/xlsxReader.ts
+++ typescript-xlsx/app/xlsxReader.ts
@@ -1,13 +1,13 @@
 const xlsx = require("xlsx");
 
-export const readExcelFile = async () => {
+const readExcelFile = async () => {
     try {
         const file = xlsx.readFile(`src/utils/ExampleSheet.xlsx`);
         let data: String[] = []
         const sheets = file.SheetNames
         for (let i = 0; i < sheets.length; i++) {
             const temp = xlsx.utils.sheet_to_json(
-                file.Sheets[file.SheetNames[i]])
+                file.Sheets[file.SheetNames[i]], {defval:""})
             temp.forEach((res: string) => {
                 data.push(res)
             })
@@ -18,4 +18,15 @@ export const readExcelFile = async () => {
     catch (err) {
         console.log(err);
     }
+}
+
+const waybillConstructor = () => {
+
+}
+
+export const getData = async () => {
+    const uploadedFile = await readExcelFile();
+    console.log('uploadedFile :', uploadedFile);
+    return uploadedFile;
+
 }
